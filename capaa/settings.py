bc5ca4a429ee0592c2306be17dd649ccfc060fe7
Chao Cheng <chaozcheng@gmail.com>
2019-01-02 11:54:31 -0500

    turn on debug

--- capaa/settings.py
+++ capaa/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(fraxsp6t@-b@8^eph7_a17aa!vzm$1scbdm*-i((+08#n2s#i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
